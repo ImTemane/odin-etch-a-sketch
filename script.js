439575df26515f6c77cc7ef3d7d5417f015b587c
ImTemane <170125706+ImTemane@users.noreply.github.com>
2025-03-01 14:42:31 +0100

    Change the bg-color of .cube when hovered
    Made possible with js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector("div.container");
 
+function RandomNumberBetween1and255(){
+  return Math.floor(Math.random() * 256);
+}
+
+function generateRandomColor() {
+  return `rgb(${RandomNumberBetween1and255()}, ${RandomNumberBetween1and255()}, ${RandomNumberBetween1and255()})`;
+}
+
 for (let i = 0; i < (16*16); i++) {
   const cube = document.createElement("div");
   cube.classList.add("cube");
   container.appendChild(cube);
+  cube.addEventListener("mouseenter", () => {
+    cube.style.backgroundColor = generateRandomColor();
+  });
 }
